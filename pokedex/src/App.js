c777b18b704a8e9bcbbc46154d40bccb17e2307f
SergioTanakaAtos <serginho.tanaka@edu.unifil.br>
2023-10-05 15:57:50 -0300

    initial commit

---
+++ pokedex/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./componentes/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <div className="App"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ pokedex/src/componentes/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <div>
+          <img alt="pokeapi-logo" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
